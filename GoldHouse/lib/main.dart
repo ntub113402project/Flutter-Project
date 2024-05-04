d561f5a85874293beaf9c9b2bf1555f4b30399c5
yuhanchennn <11046034@ntub.edu.tw>
2024-05-04 11:44:52 +0800

    create registerpage

--- GoldHouse/lib/main.dart
+++ GoldHouse/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'pages/register_page.dart';
 import 'pages/login_page.dart';
 
 void main() {
   runApp(
     MaterialApp(  
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/register':(context) => RegisterPage(),
+        '/login':(context) => LoginPage(),
+        
+      },
       home: LoginPage(),
     )
   );
--- GoldHouse/lib/pages/login_page.dart
+++ GoldHouse/lib/pages/login_page.dart
@@ -15,107 +15,106 @@ class _LoginPageState extends State<LoginPage> {
         padding: const EdgeInsetsDirectional.only(top: 0),
         child: Column(
           children: <Widget>[
-            Expanded(child: Container(
-              
-              decoration: const BoxDecoration(
-                image: DecorationImage( 
-                  image: AssetImage('assets/Logo.png',),
-                  fit: BoxFit.cover,
+            
+            Expanded(
+              flex: 3,
+              child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage( 
+                    image: AssetImage('assets/Logo.png',),
+                    fit: BoxFit.cover),
                 ),
               ),
-            ),),
+            ),
             
-            const SizedBox(height: 15,),
+            
             Expanded(
+              flex: 4,
               child: Container(
                 decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(60),
-                    topRight: Radius.circular(60),
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
                   ),
                 ),
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 50, right: 50),
+                    padding: const EdgeInsets.only(left: 40, right: 40),
                     child: ListView(
                       children:<Widget>[
-                        Container(
-                          padding: const EdgeInsets.only(left: 10, bottom: 10),
+                        SizedBox(height: 15,),
+                        Container(  
                           alignment: Alignment.centerLeft,
-                          child: const Text('電子郵件',style: TextStyle(fontSize: 25,color: Color(0xFF613F26)),),
+                          child:  Text('電子郵件',style: TextStyle(fontSize: 22,color: Color(0xFF613F26)),),
                         ),
+                        SizedBox(height: 5,),
                         Container(
-                          padding: const EdgeInsets.all(5),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(30),
                             color: const Color(0xFFEFEBE9),
                           ),
-                          child: const Padding(
+                          child: Padding(
                             padding: EdgeInsets.only(left: 10),
                             child: TextField(
                               decoration: InputDecoration(
                                 hintText: ('請輸入電子郵件'),
-                                hintStyle: TextStyle(color: Color.fromARGB(255, 128, 111, 111)),
+                                hintStyle: TextStyle( color: Color.fromARGB(255, 128, 111, 111)),
                                 border: InputBorder.none
                               ),
                             ),
                           ) 
                         ),
-                        const SizedBox(height: 20),
-                        Container(
-                          padding: const EdgeInsets.only(left: 10, bottom: 10),
+                        const SizedBox(height: 15),
+                        Align(
                           alignment: Alignment.centerLeft,
-                          child: const Text( '密碼',style: TextStyle(fontSize: 25,color: Color(0xFF613F26)),),
+                          child: Text( '密碼',style: TextStyle(fontSize: 22,color: Color(0xFF613F26)),),
                         ),
+                        SizedBox(height: 5,),
                         Container(
-                          padding: const EdgeInsets.all(5),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(30),
                             color: const Color(0xFFEFEBE9),
                           ),
-                          child: const Padding(
+                          child: Padding(
                             padding: EdgeInsets.only(left: 10),
                             child: TextField(
                               decoration: InputDecoration(
                                 hintText: ('請輸入密碼'),
-                                hintStyle: TextStyle( color: Color.fromARGB(255, 128, 111, 111)),
+                                hintStyle: TextStyle(color: Color.fromARGB(255, 128, 111, 111)),
                                 border: InputBorder.none
                               ),
                             ),
                           )
                         ),
-                        const SizedBox(height: 10,),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: GestureDetector(
-                            onTap: (){},
-                            child: const Text('忘記密碼?',style: TextStyle(color: Color(0xFF613F26)),),
-                          ), 
-                        ),
-                        const SizedBox(height:10),
+                        const SizedBox(height: 30,),
+                        
                         Align(
                           child: SizedBox(
-                            width: 150,
-                            height: 60,
+                            width: 120,
                             child: TextButton(
                               onPressed: () {},
-                              style: TextButton.styleFrom(backgroundColor: const Color(0xFFECD8C9)),
-                              child: const Text('登入',style: TextStyle(fontSize: 26,color: Colors.black),),
+                              style: TextButton.styleFrom(backgroundColor: const Color(0xFFECD8C9),),
+                              child: const Text('登入',style: TextStyle(fontSize: 20,color: Colors.black),),
                             ),
                           ),
                         ),
-                        const SizedBox(height: 15,),
-                        Align(
-                          alignment: Alignment.center,
-                          child: GestureDetector(
+                        const SizedBox(height:15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            GestureDetector(
+                            onTap: (){Navigator.pushNamed(context, '/register');},
+                            child: Text('註冊帳號',style: TextStyle(color: Color(0xFF613F26)),),),
+                            GestureDetector(
                             onTap: (){},
-                            child: const Text('還沒有帳號嗎?這裡註冊',style: TextStyle(fontSize: 15,color: Color(0xFF613F26)),),
-                          ),
-                        )
+                            child: Text('忘記密碼?',style: TextStyle(color: Color(0xFF613F26)),),),                            
+                          ],
+                        ),
+                    
                       ],
                     ),
                   ),
-                  )
+                  ),
                   
                   
               ),
